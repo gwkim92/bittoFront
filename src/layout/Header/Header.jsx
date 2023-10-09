3526ba234405740e67b9ade8d6accbf7497a74d9
kgd7054 <kgd7054@gmail.com>
2023-10-10 03:42:17 +0900

    added: scanner page

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,50 +1,55 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import NavItem from "../../Sections/NavItem";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import NavItem from '../../Sections/NavItem';
 
 const Header = () => {
-  const [menu, setMenu] = useState(false);
-  const navigate = useNavigate();
-  const userData = useSelector((state) => state.user?.userData);
-  const handleMenu = () => {
-    setMenu(!menu);
-  };
-  // Wallet 버튼 클릭 시 처리하는 함수
-  const handleWalletClick = () => {
-    if (userData?.publicKey) {
-      // 니모닉 코드가 존재하는 경우 지갑 페이지로 이동
-      navigate("/wallet");
-    } else {
-      // 니모닉 코드가 없는 경우 지갑 생성 페이지로 이동
-      navigate("/walletCreate");
-    }
-  };
+	const [menu, setMenu] = useState(false);
+	const navigate = useNavigate();
+	const userData = useSelector((state) => state.user?.userData);
+	const handleMenu = () => {
+		setMenu(!menu);
+	};
+	// Wallet 버튼 클릭 시 처리하는 함수
+	const handleWalletClick = () => {
+		if (userData?.publicKey) {
+			// 니모닉 코드가 존재하는 경우 지갑 페이지로 이동
+			navigate('/wallet');
+		} else {
+			// 니모닉 코드가 없는 경우 지갑 생성 페이지로 이동
+			navigate('/walletCreate');
+		}
+	};
 
-  return (
-    <section className="relative z-10 text-white bg-black">
-      <div className="w-full">
-        <div className="flex items-center justify-between mx-5 sm:mx-10 lg:mx-10">
-          <div className="flex items-center text-exl h-14">
-            <Link to="/">Bitto</Link>
-          </div>
-          <div className="text-2xl sm:hidden">
-            <button onClick={handleMenu}>{menu ? "-" : "+"}</button>
-          </div>
+	return (
+		<section className='relative z-10 text-white bg-black'>
+			<div className='w-full'>
+				<div className='flex items-center justify-between mx-5 sm:mx-10 lg:mx-10'>
+					<div className='flex items-center text-exl h-14'>
+						<Link to='/'>Bitto</Link>
+						<Link to='/scan' className='ml-2'>
+							Scan
+						</Link>
+					</div>
+					<div className='text-2xl sm:hidden'>
+						<button onClick={handleMenu}>{menu ? '-' : '+'}</button>
+					</div>
 
-          <div>
-            <button onClick={handleWalletClick}>Wallet</button>
-          </div>
-          <div className="hidden sm:block">
-            <NavItem />
-          </div>
-        </div>
+					<div>
+						<button onClick={handleWalletClick}>Wallet</button>
+					</div>
+					<div className='hidden sm:block'>
+						<NavItem />
+					</div>
+				</div>
 
-        {/* mobile v*/}
-        <div className="block sm:hidden">{menu && <NavItem mobile />}</div>
-      </div>
-    </section>
-  );
+				{/* mobile v*/}
+				<div className='block sm:hidden'>
+					{menu && <NavItem mobile />}
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Header;
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,31 +1,31 @@
-import axios from "axios";
+import axios from 'axios';
 
 const axionInstance = axios.create({
-  baseURL: import.meta.env.PROD ? "" : "http://localhost:4000",
+	baseURL: import.meta.env.PROD ? '' : 'http://localhost:4000',
 });
 
 axionInstance.interceptors.request.use(
-  function (config) {
-    console.log("axios interceptor test", config);
-    config.headers.Authorization =
-      "Bearer " + localStorage.getItem("accessToken");
+	function (config) {
+		console.log('axios interceptor test', config);
+		config.headers.Authorization =
+			'Bearer ' + localStorage.getItem('accessToken');
 
-    return config;
-  },
-  function (error) {
-    return Promise.reject(error);
-  }
+		return config;
+	},
+	function (error) {
+		return Promise.reject(error);
+	}
 );
 
 axionInstance.interceptors.response.use(
-  function (response) {
-    return response;
-  },
-  function (error) {
-    if (error.response.data === "jwt expired") {
-      window.location.reload();
-    }
-    return Promise.reject(error);
-  }
+	function (response) {
+		return response;
+	},
+	function (error) {
+		if (error.response.data === 'jwt expired') {
+			window.location.reload();
+		}
+		return Promise.reject(error);
+	}
 );
 export default axionInstance;
